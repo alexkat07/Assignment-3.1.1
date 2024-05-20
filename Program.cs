540872e5777406373e7bfc2a9dc35ac2cedf9b51
alexk <alexk@DESKTOP-QL7PNT4>
2024-05-20 14:20:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignment_3._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i < 51; i++) 
+            {
+                int ans = i*2;
+                Convert.ToString(ans);
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
